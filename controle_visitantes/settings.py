999231bcf9f5c9fda89a14bdc6cca9cf08f5d0bf
torineris <vitorianerisrebelo@gmail.com>
2022-08-30 13:35:35 -0300

    Criando usuarios no models, superuser e config. de idioma e fuso

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -84,6 +84,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "usuarios.Usuario"
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -107,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
