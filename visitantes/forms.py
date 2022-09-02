b49e4214f08c3eb94f32bb9fb7e62350ae7715e2
torineris <vitorianerisrebelo@gmail.com>
2022-09-02 11:05:41 -0300

    criando o form de visitantes e adicionando na view

---
+++ visitantes/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from visitantes.models import Visitante
+
+class VisitanteForm(forms.ModelForm):
+    class Meta:
+        model = Visitante
+        fields = "__all__"
--- visitantes/views.py
+++ visitantes/views.py
@@ -1,8 +1,13 @@
-import re
 from django.shortcuts import render
+from visitantes.forms import VisitanteForm
 
 def registrar_visitante(request):
 
-    context = {}
+    form = VisitanteForm()
+
+    context = {
+        "nome_pagina": "Registrar visitante",
+        "form": form
+    }
 
     return render(request, "registrar_visitante.html", context)
