edf43e4e381f144412f59e8cfbd25ee9b0fe1b99
torineris <vitorianerisrebelo@gmail.com>
2022-09-07 19:55:48 -0300

    Personalizando mensagens de erro

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -8,3 +8,19 @@ class VisitanteForm(forms.ModelForm):
             "nome_completo", "cpf", "data_nascimento",
             "numero_casa", "placa_veiculo"
         ]
+        
+        error_messages = {
+            "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para o registro"
+            },
+            "cpf": {
+                "required": "O CPF do visitante é obrigatório para o registro"
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatório para o registro",
+                "invalid": "Por favor, informe um formato válido para a data de nascimento (DD/MM/AAAA)"
+            },
+            "numero_casa": {
+                "required": "Por favor, informe o número da casa a ser visitada"
+            }
+        }
