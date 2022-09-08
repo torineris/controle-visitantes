e26e5bd29d6c810854a4e149cc9eab49cfe764ac
torineris <vitorianerisrebelo@gmail.com>
2022-09-08 11:19:30 -0300

    Adicionando a funcao "get_object_or_404" na view de visitante

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,5 +1,9 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, get_object_or_404
+    )
+
+from visitantes.models import Visitante
 from visitantes.forms import VisitanteForm
 
 def registrar_visitante(request):
@@ -30,8 +34,13 @@ def registrar_visitante(request):
     return render(request, "registrar_visitante.html", context)
 
 def informacoes_visitante(request, id):
+    visitante = get_object_or_404(
+        Visitante,
+        id=id
+    )
     context = {
         "nome_pagina": "Informações do visitante",
+        "visitante": visitante,
     }
 
     return render(request, "informacoes_visitante.html", context)
