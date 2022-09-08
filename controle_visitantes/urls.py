0b1a68e5e21e4c5f881aab59b84f531036779d24
torineris <vitorianerisrebelo@gmail.com>
2022-09-08 11:26:09 -0300

    Criando a url da pagina "informacoes_visitante"

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path
 
 from usuarios.views import index
-from visitantes.views import registrar_visitante
+from visitantes.views import (
+    registrar_visitante, informacoes_visitante
+    )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index",),
-    path("registrar-visitante/", registrar_visitante, name="registrar_visitante",)
+    path("registrar-visitante/", registrar_visitante, name="registrar_visitante",),
+    path("visitantes/<int:id>/", informacoes_visitante, name="informacoes_visitante"),
 ]
