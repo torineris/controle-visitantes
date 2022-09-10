aa6803e9eee531e2ae81167b07a89469355b1f77
torineris <vitorianerisrebelo@gmail.com>
2022-09-10 09:35:43 -0300

    Adicionando url; botão e modal  no html ao "finalizar visita"

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from usuarios.views import index
 from visitantes.views import (
-    registrar_visitante, informacoes_visitante
+    registrar_visitante, informacoes_visitante, finalizar_visita
     )
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     path("", index, name="index",),
     path("registrar-visitante/", registrar_visitante, name="registrar_visitante",),
     path("visitantes/<int:id>/", informacoes_visitante, name="informacoes_visitante"),
+    path("visitantes/<int:id>/finalizar-visita", finalizar_visita, name="finalizar_visita"),
 ]
