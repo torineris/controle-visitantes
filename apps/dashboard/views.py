4e7c640d781c0f605832a80fe9987dc59ebbc6d3
torineris <vitorianerisrebelo@gmail.com>
2022-09-11 18:24:50 -0300

    Filtrando os visitantes por status

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -3,12 +3,26 @@ from visitantes.models import Visitante
 
 def index(request):
 
-    todos_visitantes = Visitante.objects.all(
+    todos_visitantes = Visitante.objects.all()
 
+    visitantes_aguardando = todos_visitantes.filter(
+        status="AGUARDANDO"
+    )
+
+    visitantes_em_visita = todos_visitantes.filter(
+        status="EM_VISITA"
+    )
+
+    visitantes_finalizado = todos_visitantes.filter(
+        status="FINALIZADO"
     )
 
     context = {
         "nome_pagina": "Início da dashboard",
         "todos_visitantes": todos_visitantes,
+        "visitantes_aguardando": visitantes_aguardando,
+        "visitantes_em_visita": visitantes_em_visita,
+        "visitantes_finalizado": visitantes_finalizado,
     }
+
     return render(request, "index.html", context)
